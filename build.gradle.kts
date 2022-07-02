3343ac890f242514207c35836a99793a1d84fc71
Cubxity <cubxity@protonmail.com>
2022-07-02 19:12:08 +0200

    build: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.7.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -14,9 +14,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
-    implementation("com.github.steveice10:mcprotocollib:1.18.2-1")
+    implementation("com.github.steveice10:mcprotocollib:1.19-1")
     implementation("org.fusesource.jansi:jansi:2.4.0")
 }
 
