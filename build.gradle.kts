f031fc4a2d990391965d9cffb93a7e0cee8ffeab
Cubxity <contact@cubxity.dev>
2022-09-03 10:34:42 +0200

    chore: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.5")
     implementation("com.github.steveice10:mcprotocollib:1.19-1")
     implementation("org.fusesource.jansi:jansi:2.4.0")
 }
